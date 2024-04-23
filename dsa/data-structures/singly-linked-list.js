04193b0939d0b184f779ff5cd9d4b55837bb84a2
conrad93 <nunes.conrad93@gmail.com>
2024-04-23 11:45:11 +0530

    sll video fin

--- dsa/data-structures/singly-linked-list.js
+++ dsa/data-structures/singly-linked-list.js
@@ -12,6 +12,40 @@ class SinglyLinkedList {
         this.length = 0;
     }
 
+    insert(index, val) {
+        if(index < 0 || index > this.length) return false;
+        if(index === this.length) return !!this.push(val);
+        if(index === 0) return !!this.unshift(val);
+
+        let newNode = new Node(val);
+        let prev = this.get(index - 1);
+        let temp = prev.next;
+        prev.next = newNode;
+        newNode.next = temp;
+        this.length++;
+        return true;
+    }
+
+    get(index) {
+        if(index < 0 || index >= this.length) return null;
+        let counter = 0;
+        let current = this.head;
+        while(counter !== index){
+            current = current.next;
+            counter++;
+        }
+        return current;
+    }
+
+    set(index, val) {
+        let foundNode = this.get(index);
+        if(foundNode) {
+            foundNode.value = val;
+            return true;
+        }
+        return false;
+    }
+
     push(val) {
         let newNode = new Node(val);
         if(!this.head) {
@@ -66,12 +100,48 @@ class SinglyLinkedList {
         this.length++;
         return this;
     }
+
+    remove(index) {
+        if(index < 0 || index >= this.length) return undefined;
+        if(index === 0) return this.shift();
+        if(index === this.length - 1) return this.pop();
+
+        let previousNode = this.get(index - 1);
+        let removed = previousNode.next;
+        previousNode.next = removed.next;
+        this.length--;
+        return removed;
+    }
+
+    reverse() {
+        let node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+        let next;
+        let prev = null;
+        for(let i = 0; i < this.length; i++) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        return this;
+    }
+
+    print() {
+        let arr = [];
+        let current = this.head;
+        while(current) {
+            arr.push(current.value);
+            current = current.next;
+        }
+        console.log(arr);
+    }
 }
 
 let list = new SinglyLinkedList();
-list.push("Hello");
-list.push("World");
 list.push("!");
-console.log(list);
-console.log(list.pop());
-console.log(list);
+list.unshift("Hello");
+list.insert(1, "World");
+list.reverse();
+list.print();
