71d0d8cbae14a1e4567c954b4257276c08a00ef3
conrad93 <nunes.conrad93@gmail.com>
2024-04-23 23:19:55 +0530

    py day 1

---
+++ python/daily-code/day-1/main.py
@@ -0,0 +1,10 @@
+# print("Hello World!\nThis is my first day of 100 days of code challenge." + " " + "I am excited to learn new things.")
+# input("What is your name?")
+
+# name = input()
+# print(len(name))
+
+print("Welcome to the Band Name Generator.")
+city = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
